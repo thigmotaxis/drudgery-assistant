66b251f92e2bf55542593db56d9091923555466f
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-08-19 07:58:42 -0700

    change default title for compiled page to Drudgery Assistant

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development',
+      title: 'Drudgery Assistant',
     }),
   ],
   output: {
