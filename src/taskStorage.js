e41b507f87b1c51be4e81c7492c92a6fbae8642a
Andrew Jennison <andrewjohnjennison@gmail.com>
2022-08-21 11:30:24 -0700

    refactor taskStorage into a module containing functions to store objects and retrieve taskList array

--- src/taskStorage.js
+++ src/taskStorage.js
@@ -1,6 +1,13 @@
-const taskList = [];
-
-export function receiveTask(object) {
-  taskList.push(object);
-  return taskList;
-}
+export const storage = (() => {
+  const taskList = [];
+  const storeTask = (obj) => {
+    taskList.push(obj)
+  };
+  const getTaskList = () => {
+    return taskList;
+  };
+  const sortTaskList = (category) => {
+    // THIS FUNCTION WILL SORT TASK LIST BASED ON THE SPECIFIED CATEGORY (e.g. DATE OR PROJECT), WILL LIKELY HAVE TO REWORK getTaskList
+  };
+  return {storeTask, getTaskList}
+})();
